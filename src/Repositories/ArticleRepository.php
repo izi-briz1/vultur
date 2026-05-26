584d56310c5150653475924338d11d94bad54ddf
Sergey K <285973516+izi-briz1@users.noreply.github.com>
2026-05-26 13:58:19 +0300

    create repository stabs classes

---
+++ src/Repositories/ArticleRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+use PDO;
+
+class ArticleRepository
+{
+    /**
+     * @var PDO
+     */
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+}
---
+++ src/Repositories/CategoryRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+use PDO;
+
+class CategoryRepository
+{
+    /**
+     * @var PDO
+     */
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+}
